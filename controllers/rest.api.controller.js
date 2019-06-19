4f8fe131ba6181bad683684092e3569504f7bf02
ilkemerol <ilkemerol@hotmail.com>
2019-06-19 15:15:12 +0300

    response fix

--- controllers/rest.api.controller.js
+++ controllers/rest.api.controller.js
@@ -3,7 +3,8 @@ const restApiService = require("../services/rest.api.service");
 exports.createRestApi = async function(req, res, next) {
   var serviceResponse = await restApiService.createRestApi(req);
   return res.status(200).json({
-    endpoint: "/" + serviceResponse
+    platform: "node",
+    endpoint: serviceResponse
   });
 };
 
--- services/rest.api.service.js
+++ services/rest.api.service.js
@@ -3,18 +3,18 @@ const fs = require("fs");
 
 exports.createRestApi = async function(req) {
   var userRestApi = req.body;
-  const uuid = randomstring.generate({ length: 7, charset: "alphabetic" });
+  const uuid = randomstring.generate({ length: 32, charset: "alphabetic" });
   userRestApi = userRestApi.replace("uuid", uuid);
-  fs.mkdir("./public/" + uuid, { recursive: true }, err => {
+  fs.mkdirSync("./codes/" + uuid, { recursive: true }, err => {
     if (err) throw err;
   });
-  fs.appendFileSync("./public/" + uuid + "/" + uuid + ".js", userRestApi);
+  fs.appendFileSync("./codes/" + uuid + "/" + uuid + ".js", userRestApi);
   return uuid;
 };
 
 exports.callRestApi = async function(req) {
   const uuid = req.params.hash;
-  const userRestService = require("../public/" + uuid + "/" + uuid);
+  const userRestService = require("../codes/" + uuid + "/" + uuid);
   const json = userRestService[uuid](req);
   return json;
 };
